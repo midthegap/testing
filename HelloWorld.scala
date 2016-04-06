5dd4cfc57eabb2a8facf10edb0956a1cb5e88c9d
Bob <stefano.peruzzi@magentalab.it>
2016-04-06 08:48:20 +0200

    featureX

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -4,4 +4,12 @@ object HelloWorld extends App {
 
 	println("Il numero segreto e' : " + scala.util.Random.nextInt(10))
 	println("Bob e' un grande programmatore")
+
+	println("Il risultato della feature x e' " + featureX)
+
+	def featureX(): Double = {
+		val num = scala.util.Random.nextDouble()
+		val den = scala.util.Random.nextDouble()
+		num / den
+	}
 }
